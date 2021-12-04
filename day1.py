eb2298af334b1feb51f9f8b0ff1a2d6673387394
Anna Knoerr <an.kn@protonmail.ch>
2021-12-04 01:45:39 +0100

    day 1

--- day1.py
+++ day1.py
@@ -1,2 +1,10 @@
-msg = "Hello world"
-print(msg)
+import numpy as np
+
+data = np.loadtxt('day1_input')
+
+diff = np.diff(data)
+print(len(diff))
+
+log = [1 for x in diff if x > 0]
+print(np.sum(log))
+
